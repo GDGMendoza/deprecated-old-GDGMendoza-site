14a320a451e05b19b91f46e2cb5de83872b6b8b6
Cristian <matrixcmr@gmail.com>
2013-09-20 17:58:20 -0300

    Agregada parte de la estructura de cloud endpoints...

---
+++ api/models_cloud/post_model.py
@@ -0,0 +1,30 @@
+#  coding: utf-8 --
+from google.appengine.ext import ndb
+from endpoints_proto_datastore.ndb import EndpointsAliasProperty
+from endpoints_proto_datastore.ndb import EndpointsModel
+from api.models.contributor_model import Contributor
+
+class Comment(EndpointsModel):
+
+    author = ndb.KeyProperty(kind=Contributor)
+    content = ndb.TextProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+
+class Post(EndpointsModel):
+
+    title = ndb.StringProperty()
+    author = ndb.KeyProperty(kind=Contributor)
+    description = ndb.StringProperty()
+    content = ndb.TextProperty()
+    cover = ndb.StringProperty()
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    tags = ndb.StringProperty(repeated=True)
+    comments = ndb.StructuredProperty(Comment, repeated=True)
+
+    def IdSet(self, value):
+        self.UpdateFromKey(ndb.Key(Post, str(value)))
+
+    @EndpointsAliasProperty(setter=IdSet, required=True)
+    def id(self):
+        if self.key is not None:
+            return self.key.string_id()
